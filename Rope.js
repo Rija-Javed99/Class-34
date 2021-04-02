6fb8d2d8c30dda8682699c4d19e9c559afdf5366
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-04-02 14:57:42 +0530

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -1,20 +1,17 @@
 class Rope {
   constructor(bodyA, pointB) {
-    var options = {
-      bodyA: bodyA,
-      pointB: pointB,
-      stiffness: 1.2,
-      length: 250,
-    };
+    
+    
+    
+    
 
   }
   
   display() {
-    if (this.rope.bodyA) {
-      
-      
-      
-      
-    }
+    
+    
+    
+    
+    
   }
 }
